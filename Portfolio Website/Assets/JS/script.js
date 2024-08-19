5f0598bff84790200c24794e2b7ca6eba781838c
ganesh-dev01 <ganeshsaha616@gmail.com>
2024-08-19 23:43:48 +0530

    under development

--- Portfolio Website/Assets/JS/script.js
+++ Portfolio Website/Assets/JS/script.js
@@ -1,25 +1,18 @@
 let menu = document.getElementById("menubar");
 let nav_items = document.getElementById("P_nav-items");
 let close_icon = document.getElementById("close_icon");
-
-let bar_lines = document.getElementsByClassName("line");
+let bar_lines = document.querySelectorAll(".line");
+let menu_item = document.querySelectorAll(".menu-item");
 
 menu.addEventListener("click", function () {
-    if (nav_items.classList.contains("show")) {
-        nav_items.classList.remove("show");
-        setTimeout(() => {
-            nav_items.style.display = "none";
-            for (let i = 0; i < bar_lines.length; i++) {
-                bar_lines[i].style.display = "block";
-            }
-        }, 500);
-        close_icon.style.display = "none";
-    } else {
+    if (nav_items.style.display === "none" || nav_items.style.display === "") {
         nav_items.style.display = "block";
-        setTimeout(() => nav_items.classList.add("show"), 0);
+        bar_lines.forEach((line) => line.style.display = "none");
         close_icon.style.display = "block";
-        for (let i = 0; i < bar_lines.length; i++) {
-            bar_lines[i].style.display = "none";
-        }
+    } else {
+        nav_items.style.display = "none";
+        bar_lines.forEach(line => line.style.display = "block");
+        close_icon.style.display = "none";
+        window.location.reload();
     }
 });
