7deac8c45204de6b9955ae734b414ee9f618d238
ganesh-dev01 <ganeshsaha616@gmail.com>
2024-09-22 21:35:38 +0530

    under development

---
+++ Thrill_Trails_India using React/src/Pages/CMS/Destination/images.jsx
@@ -0,0 +1,118 @@
+import Victoria from '../../Images/Victoria.jpg';
+import Howrahbridge from '../../Images/HowrahBridge.jpg';
+import Dakshineswar from '../../Images/Dakshineswar.jpg';
+import IndianMuseum from '../../Images/IndianMuseum.jpg';
+import BelurMath from '../../Images/BelurMath.jpg';
+import ScienceCity from '../../Images/ScienceCity.jpg';
+import MarblePalace from '../../Images/MarblePalace.jpg';
+import St_Pauls_Cathedral from '../../Images/St_Pauls_Cathedral.jpg';
+import prinsep_ghat from '../../Images/prinsep-ghat.jpg';
+import jorasanko_thakur_bari from '../../Images/jorasanko_thakur_bari.jpg';
+import Eco_park from '../../Images/Eco_park.jpg';
+import AliporeJailMuseum from '../../Images/AliporeJailMuseum.jpg';
+
+import marina_beach from '../../Images/marina_beach.jpg';
+import KapaleeshwararTemple from '../../Images/KapaleeshwararTemple.jpg';
+import fort_st_Georges from '../../Images/fort_st_Georges.jpg';
+import SanThomeBasilica from '../../Images/SanThomeBasilica.jpg';
+import Government_Museum from '../../Images/Government_Museum.jpg';
+import ValluvarKottam from '../../Images/ValluvarKottam.jpg';
+import ElliotsBeach from '../../Images/ElliotsBeach.jpg';
+import GuindyNationalPark from '../../Images/GuindyNationalPark.jpg';
+import ArignarAnnaZoologicalPark from '../../Images/ArignarAnnaZoologicalPark.jpg';
+import ThousandLightsMosque from '../../Images/ThousandLightsMosque.jpg';
+
+import LalbaghBotanicalGarden from '../../Images/LalbaghBotanicalGarden.jpg';
+import BangalorePalace from '../../Images/BangalorePalace.jpg';
+import CubbonPark from '../../Images/CubbonPark.jpg';
+import ISKCONTemple from '../../Images/ISKCONTemple.jpg';
+import TipuSultansSummerPalace from '../../Images/TipuSultansSummerPalace.jpg';
+import BannerghattaNationalPark from '../../Images/BannerghattaNationalPark.jpg';
+import VidhanaSoudha from '../../Images/VidhanaSoudha.jpg';
+import NandiHills from '../../Images/NandiHills.jpg';
+import InnovativeFilmCity from '../../Images/InnovativeFilmCity.jpg';
+import CommercialStreet from '../../Images/CommercialStreet.jpg';
+
+
+import IndiaGate from '../../Images/IndiaGate.jpg';
+import RedFort from '../../Images/RedFort.jpg';
+import QutubMinar from '../../Images/QutubMinar.jpg';
+import LotusTemple from '../../Images/LotusTemple.jpg';
+import HumayunsTomb from '../../Images/HumayunsTomb.jpg';
+import JamaMasjid from '../../Images/JamaMasjid.jpg';
+import AkshardhamTemple from '../../Images/AkshardhamTemple.jpg';
+import RashtrapatiBhavan from '../../Images/RashtrapatiBhavan.jpeg';
+import ChandniChowk from '../../Images/ChandniChowk.jpg';
+import LodhiGardens from '../../Images/LodhiGardens.jpg';
+
+
+import GatewayofIndia from '../../Images/GatewayofIndia.jpg';
+import MarineDrive from '../../Images/MarineDrive.jpg';
+import ChhatrapatiShivaji from '../../Images/Chhatrapati_shivaji.jpg';
+import ElephantaCaves from '../../Images/ElephantaCaves.jpg';
+import JuhuBeach from '../../Images/JuhuBeach.jpg';
+import SanjayGandhiNationalPark from '../../Images/SanjayGandhiNationalPark.jpg';
+import HajiAliDargah from '../../Images/HajiAliDargah.jpg';
+import ChorBazaar from '../../Images/ChorBazaar.jpg';
+import SiddhivinayakTemple from '../../Images/SiddhivinayakTemple.jpg';
+import ColabaCauseway from '../../Images/ColabaCauseway.jpg';
+
+
+import Charminar from '../../Images/Charminar.jpg';
+import GolcondaFort from '../../Images/GolcondaFort.jpg';
+import HussainSagarLake from '../../Images/HussainSagarLake.jpg';
+import RamojiFilmCity from '../../Images/RamojiFilmCity.jpg';
+import ChowmahallaPalace from '../../Images/ChowmahallaPalace.jpg';
+import MeccaMasjid from '../../Images/MeccaMasjid.jpg';
+import BirlaMandir from '../../Images/BirlaMandir.jpg';
+import SalarJungMuseum from '../../Images/SalarJungMuseum.jpg';
+import NehruZoologicalPark from '../../Images/NehruZoologicalPark.jpg';
+import LumbiniPark from '../../Images/LumbiniPark.jpg';
+
+import SabarmatiAshram from '../../Images/SabarmatiAshram.jpeg';
+import SardarPatelStadium from '../../Images/SardarPatelStadium.avif';
+import JamaMasjid2 from '../../Images/JamaMasjid2.jpg';
+import AdalajStepwell from '../../Images/AdalajStepwell.jpg';
+import AkshardhamTemple2 from '../../Images/AkshardhamTemple2.jpg';
+import KankariaLake from '../../Images/KankariaLake.jpg';
+import ShreyasFolkMuseum from '../../Images/ShreyasFolkMuseum.jpg';
+import BhadraFort from '../../Images/BhadraFort.jpg';
+import NarmadaCanal from '../../Images/NarmadaCanal.jpg';
+import SidiSaiyyedMosque from '../../Images/SidiSaiyyedMosque.jpg';
+
+
+
+import AgaKhanPalace from '../../Images/AgaKhanPalace.jpeg';
+import ShaniwarWada from '../../Images/ShaniwarWada.jpg';
+import SinhagadFort from '../../Images/SinhagadFort.jpg';
+import OshoAshram from '../../Images/OshoAshram.jpg';
+import PuneOkayama from '../../Images/PuneOkayama.jpg';
+import RajaDinkar from '../../Images/RajaDinkar.jpg';
+import ParvatiHill from '../../Images/ParvatiHill.jpg';
+import KatrajSnakePark from '../../Images/KatrajSnakePark.jpeg';
+import FergussonCollege from '../../Images/FergussonCollege.jpeg';
+import MulshiLake from '../../Images/MulshiLake.jpg';
+
+
+const images = {
+    Victoria, Howrahbridge, Dakshineswar, IndianMuseum, BelurMath, ScienceCity, MarblePalace,
+    St_Pauls_Cathedral, prinsep_ghat, jorasanko_thakur_bari, Eco_park, AliporeJailMuseum, marina_beach,
+    KapaleeshwararTemple, fort_st_Georges, SanThomeBasilica, Government_Museum, ValluvarKottam, ElliotsBeach,
+    GuindyNationalPark, ArignarAnnaZoologicalPark, ThousandLightsMosque, LalbaghBotanicalGarden, BangalorePalace,
+    CubbonPark, ISKCONTemple, TipuSultansSummerPalace, BannerghattaNationalPark, VidhanaSoudha, NandiHills,
+    InnovativeFilmCity, CommercialStreet, IndiaGate, RedFort, QutubMinar, LotusTemple, HumayunsTomb, JamaMasjid,
+    AkshardhamTemple, RashtrapatiBhavan, ChandniChowk, LodhiGardens, GatewayofIndia, MarineDrive,
+    ChhatrapatiShivaji, ElephantaCaves, JuhuBeach, SanjayGandhiNationalPark, HajiAliDargah, ChorBazaar,
+    SiddhivinayakTemple, ColabaCauseway, Charminar, GolcondaFort, HussainSagarLake, RamojiFilmCity,
+    ChowmahallaPalace, MeccaMasjid, BirlaMandir, SalarJungMuseum, NehruZoologicalPark, LumbiniPark,
+    AgaKhanPalace, ShaniwarWada, SinhagadFort, OshoAshram, PuneOkayama, RajaDinkar, ParvatiHill,
+    KatrajSnakePark, FergussonCollege, MulshiLake, SabarmatiAshram, SardarPatelStadium, JamaMasjid2,
+    AdalajStepwell, AkshardhamTemple2, KankariaLake, ShreyasFolkMuseum,
+    BhadraFort, NarmadaCanal, SidiSaiyyedMosque
+};
+
+
+
+export default images;
+
+
