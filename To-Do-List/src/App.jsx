82fa67b13612c6836e0b8c44a0274d59d499b236
ganesh-dev01 <ganeshsaha616@gmail.com>
2024-09-29 20:43:54 +0530

    under development

---
+++ To-Do-List/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import To_Do from './To_Do'
+
+function App() {
+    return (
+        <div>
+            <To_Do />
+        </div>
+    )
+}
+
+export default App
---
+++ To-Do-List/src/To_Do.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './To_Do.css'
+function To_Do() {
+    return (
+        <div>
+            <div className="container">
+                <div className="head">
+                    <h4>To-Do-List</h4>
+                </div>
+                <div className="input_area">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default To_Do
