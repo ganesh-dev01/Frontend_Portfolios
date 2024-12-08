7ec36a731b417cbcca49ae15f365965be0de7d79
ganesh-dev01 <ganeshsaha616@gmail.com>
2024-12-09 01:26:27 +0530

    01

--- Zenithal using NEXT,TS,React/Layouts/Header/index.tsx
+++ Zenithal using NEXT,TS,React/Layouts/Header/index.tsx
@@ -1,23 +1,79 @@
-import ThemeContext from "@/ThemeContext/ThemeContext"
-import { useContext } from "react";
-import styles from '@/styles/Header.module.css'
+import React, { useContext, useState } from "react";
+import ThemeContext from "@/ThemeContext/ThemeContext";
+import { AppBar, Toolbar, IconButton, Typography, Button, Drawer, List, ListItem, ListItemText } from "@mui/material";
+import { FaSun, FaMoon, FaUserCircle, FaBars } from "react-icons/fa";
+import styles from "@/styles/Header.module.css";
 
 const Header: React.FC = () => {
-
     const theme_data = useContext<any>(ThemeContext);
+    const [mobileOpen, setMobileOpen] = useState(false);
+
+    // Toggle theme function
+    const toggleTheme = () => {
+        theme_data.setTheme(theme_data.theme === "light" ? "dark" : "light");
+    };
+
+    // Handle mobile menu toggle
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
 
     return (
-        <div className={styles[`container-${theme_data.theme}`]}>
+        <AppBar position="static" className={`${styles[`container-${theme_data.theme}`]}`}>
+            <Toolbar className={styles.toolbar}>
+                {/* Brand Name */}
+                <Typography variant="h6" className={styles.brand}>
+                    MyBrand
+                </Typography>
 
-            <ul className={styles.list}>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Products</li>
-            </ul>
+                {/* Desktop Navigation */}
+                <ul className={`${styles.list}`}>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Products</li>
+                </ul>
 
-        </div>
-    )
-}
+                {/* Action Buttons */}
+                <div className={styles.buttons}>
+                    <IconButton onClick={toggleTheme} color="inherit">
+                        {theme_data.theme === "light" ? <FaMoon /> : <FaSun />}
+                    </IconButton>
+                    <Button startIcon={<FaUserCircle />} className={styles.profileButton}>
+                        Profile
+                    </Button>
 
-export default Header
+                    {/* Mobile Menu Toggle */}
+                    <IconButton onClick={handleDrawerToggle} color="inherit" className={styles.mobileMenuButton}>
+                        <FaBars />
+                    </IconButton>
+                </div>
+            </Toolbar>
+
+            {/* Mobile Drawer */}
+            <Drawer
+                anchor="right"
+                open={mobileOpen}
+                onClose={handleDrawerToggle}
+                classes={{ paper: styles.drawer }}
+            >
+                <List>
+                    <ListItem button onClick={handleDrawerToggle}>
+                        <ListItemText primary="Home" />
+                    </ListItem>
+                    <ListItem button onClick={handleDrawerToggle}>
+                        <ListItemText primary="About" />
+                    </ListItem>
+                    <ListItem button onClick={handleDrawerToggle}>
+                        <ListItemText primary="Contact" />
+                    </ListItem>
+                    <ListItem button onClick={handleDrawerToggle}>
+                        <ListItemText primary="Products" />
+                    </ListItem>
+                </List>
+            </Drawer>
+        </AppBar>
+    );
+};
+
+export default Header;
