6b7cea859437832a198a59859df2fe51d7385d81
Ganesh saha <ganeshsaha616@gmail.com>
2024-12-09 19:50:10 +0530

    under development

--- Zenithal using NEXT,TS,React/Layouts/Footer/index.tsx
+++ Zenithal using NEXT,TS,React/Layouts/Footer/index.tsx
@@ -1,10 +1,48 @@
+import React, { useContext } from "react";
+import ThemeContext from "@/ThemeContext/ThemeContext";
+import { Box, Typography, IconButton } from "@mui/material";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import styles from "@/styles/Footer.module.css";
 
 const Footer: React.FC = () => {
-    return (
-        <div>
-            <h4>Footer</h4>
-        </div>
-    )
-}
+    const theme_data = useContext<any>(ThemeContext);
 
-export default Footer
+    return (
+        <Box className={`${styles[`footer-container-${theme_data.theme}`]}`}>
+            {/* Footer Content */}
+            <Typography variant="body1" className={styles.brand}>
+                MyBrand © {new Date().getFullYear()}
+            </Typography>
+
+            {/* Social Media Links */}
+            <Box className={styles.socialIcons}>
+                <IconButton
+                    aria-label="Facebook"
+                    href="https://www.facebook.com"
+                    target="_blank"
+                    className={styles.iconButton}
+                >
+                    <FaFacebook />
+                </IconButton>
+                <IconButton
+                    aria-label="Instagram"
+                    href="https://www.instagram.com"
+                    target="_blank"
+                    className={styles.iconButton}
+                >
+                    <FaInstagram />
+                </IconButton>
+                <IconButton
+                    aria-label="LinkedIn"
+                    href="https://www.linkedin.com"
+                    target="_blank"
+                    className={styles.iconButton}
+                >
+                    <FaLinkedin />
+                </IconButton>
+            </Box>
+        </Box>
+    );
+};
+
+export default Footer;
