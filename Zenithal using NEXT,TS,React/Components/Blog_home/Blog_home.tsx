3290eb637a90bd42c9eece490d8cced0ac69e388
ganesh-dev01 <ganeshsaha616@gmail.com>
2024-12-17 01:46:37 +0530

    under development

--- Zenithal using NEXT,TS,React/Components/Blog_home/Blog_home.tsx
+++ Zenithal using NEXT,TS,React/Components/Blog_home/Blog_home.tsx
@@ -3,10 +3,16 @@ import styles from '@/styles/component.module.css'
 import ThemeContext from '@/ThemeContext/ThemeContext';
 
 const Blog_home = () => {
-    const theme_data=useContext<any>(ThemeContext)
+    const theme_data = useContext<any>(ThemeContext)
     return (
-        <div className={styles[`blogContainer${theme_data.theme}`]}>
-            <h1>Blog_home</h1>
+        <div className={styles[`blogContainer_${theme_data.theme}`]}>
+            <div className={styles.postContainer}>
+                <div className={styles.post}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas veritatis recusandae voluptatibus molestias facilis necessitatibus odio. Debitis exercitationem illum sapiente eius quam culpa ipsum error at ipsa velit voluptatibus, dignissimos magni maxime voluptate autem eaque repellendus incidunt voluptatem et? Non qui quo reiciendis quas enim cum nemo nihil necessitatibus magnam.
+                </div>
+                <div className={styles.imageContainer}>
+                </div>
+            </div>
         </div>
     );
 };
