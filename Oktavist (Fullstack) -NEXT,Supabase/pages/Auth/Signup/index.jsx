f71f27b3563d176995d9e5d661a993f020006b0b
ganesh-dev01 <ganeshsaha616@gmail.com>
2025-01-19 19:19:45 +0530

    03

--- Oktavist (Fullstack) -NEXT,Supabase/pages/Auth/Signup/index.jsx
+++ Oktavist (Fullstack) -NEXT,Supabase/pages/Auth/Signup/index.jsx
@@ -1,11 +1,11 @@
 import Theme_button from '@/Components/Theme_button';
-import styles from '@/styles/Auth_styles/signin.module.css';
+import styles from '@/styles/Auth_styles/signup.module.css';
 import { ThemeContext } from '@/Theme/Themestate';
 import { useContext } from 'react';
 import { TextField, Button, Typography, Box } from '@mui/material';
 import google_icon from '@/public/assets/icon/google_icon.png';
 
-const Signin = () => {
+const Signup = () => {
     const theme_data = useContext(ThemeContext);
 
     return (
@@ -14,12 +14,18 @@ const Signin = () => {
 
             <div className={styles[`container_${theme_data.theme}`]}>
 
-                {/* Sign-in Form */}
+                {/* Signup Form */}
                 <Typography variant="h4" className={styles.heading}>
-                    Sign In
+                    Sign Up
                 </Typography>
 
                 <Box component="form" className={styles.form}>
+                    <TextField
+                        label="Name"
+                        fullWidth
+                        className={styles.input_field}
+                        id={styles[`input_field_${theme_data.theme}`]}
+                    />
                     <TextField
                         label="Email"
                         fullWidth
@@ -33,32 +39,31 @@ const Signin = () => {
                         className={styles.input_field}
                         id={styles[`input_field_${theme_data.theme}`]}
                     />
-                    <Typography className={styles.forgot_password}>
-                        Forgot your password?
-                    </Typography>
+                    <TextField
+                        label="Confirm Password"
+                        type="password"
+                        fullWidth
+                        className={styles.input_field}
+                        id={styles[`input_field_${theme_data.theme}`]}
+                    />
 
-                    <Box className={styles.signin_btn_container}>
-
-                        <Button className={styles.google_btn}>
-                            <img src={google_icon.src} alt="Google Icon" />
-                        </Button>
-
+                    <Box className={styles.signup_btn_container}>
                         <Button
                             variant="contained"
                             color="primary"
-                            className={styles.signin_btn}
+                            className={styles.signup_btn}
                         >
-                            Sign In
+                            Sign Up
                         </Button>
                     </Box>
                 </Box>
 
                 <Typography variant="body2" className={styles.helper_text}>
-                    Don't have an account? <span className={styles[`span_text_${theme_data.theme}`]}>Sign up</span>
+                    Already have an account? <span className={styles[`span_text_${theme_data.theme}`]}>Sign in</span>
                 </Typography>
             </div>
         </div>
     );
 };
 
-export default Signin;
+export default Signup;
