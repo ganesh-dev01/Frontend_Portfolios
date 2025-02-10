ea0deb751f23a7748ccfbb0a994b640491752988
ganesh-dev01 <ganeshsaha616@gmail.com>
2025-02-10 22:19:11 +0530

    context api created

--- To-Do  -NEXT,tRPC,TS,Prisma,SQLite/pages/_app.tsx
+++ To-Do  -NEXT,tRPC,TS,Prisma,SQLite/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
+import { ThemeState } from "@/Theme/Themestate";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ThemeState>
+        <Component {...pageProps} />
+      </ThemeState>
+    </>
+  )
+
 }
