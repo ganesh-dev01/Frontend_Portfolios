71e3dce0edb59973fe967a31fcffc1fd71685230
ganesh-dev01 <ganeshsaha616@gmail.com>
2025-02-11 10:05:46 +0530

    ui

--- To-Do  -NEXT,tRPC,TS,Prisma,SQLite/pages/user/cms/dashboard/index.tsx
+++ To-Do  -NEXT,tRPC,TS,Prisma,SQLite/pages/user/cms/dashboard/index.tsx
@@ -5,14 +5,30 @@ import styles from '@/styles/user_dashboard.module.css';
 const Dashboard: React.FC = () => {
     const data = useContext(ThemeContext);
     const { theme } = data || {};
+    // const theme='dark';
 
     return (
         <div className={`${styles[`main_${theme}`]} ${styles.main}`}>
+            <div className={styles.upperSection}>
 
-            <div className={styles.upperNav}>
                 <div className={styles.taskbox}>
 
+                    <div className={styles.input_area}>
+                        <label className={styles.inputlabel}>Tittle</label>
+                        <input type="text" className={styles.taskinput} placeholder="Enter Task" />
+                    </div>
+
+                    <div className={styles.input_area}>
+                        <label className={styles.inputlabel}>Description</label>
+                        <textarea className={styles.description} placeholder="Enter Description"></textarea>
+                    </div>
+                    
+                    <div className={styles.btn_area}>
+                        <button className={styles.addbtn}>Add Task</button>
+                    </div>
+
                 </div>
+
                 <div className={styles.accountbox}>
 
                 </div>
