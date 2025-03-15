f9b3d10daaaaee4763b21ae720fbc2bc853e51f9
Ganesh saha <ganeshsaha616@gmail.com>
2025-03-15 11:39:39 +0530

    initialized

---
+++ OnTrip -NEXT,TS/pages/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/Components/Navbar"
+
+const Home:React.FC=()=>{
+    return(
+        <div>
+          <Navbar />
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
